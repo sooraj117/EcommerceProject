79ffd5e34251f8293e7650590576f5a126cdbbf8
sooraj117 <soorajbab2@gmail.com>
2020-07-29 22:46:14 +0530

    dsfsf

--- src/test/java/sampleprojectMaven/FailedAll.java
+++ src/test/java/sampleprojectMaven/FailedAll.java
@@ -2,10 +2,17 @@
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.Set;
 
 import org.testng.IAnnotationTransformer;
 import org.testng.IRetryAnalyzer;
 import org.testng.annotations.ITestAnnotation;
+import org.testng.annotations.Test;
 
 public class FailedAll implements IAnnotationTransformer {
 
@@ -17,6 +24,51 @@ public class FailedAll implements IAnnotationTransformer {
 			annotation.setRetryAnalyzer(Failed.class);
 		}
 		
+	}
+	
+	
+	public void verifyimageActive(WebElement imgElement) {
+		try {
+			HttpClient client = HttpClientBuilder.create().build();
+			HttpGet request = new HttpGet(imgElement.getAttribute("src"));
+			HttpResponse response = client.execute(request);
+			// verifying response code he HttpStatus should be 200 if not,
+			// increment as invalid images count
+			if (response.getStatusLine().getStatusCode() != 200)
+				invalidImageCount++;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void tc2 () {
+		   HashMap<Integer, String> lli = new HashMap<Integer, String>();
+		lli.put(12212, "sffd");
+		lli.put(435, "gfhf");
+		lli.put(654, "sf");
+		lli.put(1321, "fgsdg");
+		
+	Set<Entry<Integer,String>> s=lli.entrySet();
+	for (Entry<Integer, String> entry : s) {
+		entry.getKey();                                       
+	}
+	}
+	@Test
+	public void tc1() {
+		Realp r1 = Realp.add();
+		r1.sample();
+		
+		  List< Integer > llll = new ArrayList<Integer>();
+		  llll.add(435);
+		  llll.add(98);
+		  
+		  for(int i=0;i<=llll.size();i++) {
+			  int ddd = llll.get(i);
+			  
+			  
+			  
+		  }
 	}
 
 }
